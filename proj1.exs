fb79b05fb602d87ae053c5439c045ecd91347ed2
Kayton <kaytonfletcher@gmail.com>
2019-08-26 20:00:25 -0400

    Added check for 2 command line arguments and function to calculate permutations

--- proj1.exs
+++ proj1.exs
@@ -3,12 +3,20 @@ defmodule Proj1 do
   Documentation for Proj1.
   """
 
+  # base case for empty list
+  def permutations([], _n), do: []
+  # base case for zero-length permutation
+  def permutations(_li, 0), do: []
 
+  def permutations(li, n), do: for hd <- li, tl <- permutations(li, n-1), do: [hd | tl]
 
-
-  def hello do
-    IO.inspect System.argv()
+  def start do
+    if(length(System.argv()) == 2) do
+      IO.inspect System.argv()
+    else
+      IO.puts("Bad Arguments")
+    end
   end
 end
 
-Proj1.hello()
+Proj1.start()
